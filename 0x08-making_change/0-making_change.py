a9f0bba9882343c3faebb58958bf53c763b0f4f9
Philip Ajuong Deng <philipajuong@gmail.com>
2024-11-28 18:02:17 +0200

    0x08. Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,22 +2,23 @@
 """ Making changes """
 
 def makeChange(coins, total):
-    """
-    Determine the fewest number of coins needed to meet a given total.
-    
-    :param coins: List[int] - List of the values of the coins
-    :param total: int - The target total
-    :return: int - Fewest number of coins needed, or -1 if not possible
+    """ Generate changes needed to reach total
+
+    Args:
+        coins ([List]): [List of Coins available]
+        total ([int]): [total amount needed]
     """
     if total <= 0:
         return 0
-    
-    # Initialize a DP array with a value greater than the maximum possible number of coins
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: 0 coins needed to make a total of 0
-    
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-    
-    return dp[total] if dp[total] != float('inf') else -1
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
